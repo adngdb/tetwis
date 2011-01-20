3cdcd2923e7284329d5820747aea82d2408ec63b
AdrianGaudebert <adrian@gaudebert.fr>
2011-01-20 18:09:05 +0100

    Changing colors of the players.

--- client/js/main.js
+++ client/js/main.js
@@ -7,5 +7,9 @@ $(document).ready(function() {
 
         $('#map').width(width).height(height);
         $('#players').width(width);
+
+        for (var i = 0; i < game.config.players.number; i++) {
+            $('#p'+(i+1)).css('color', game.config.players.colors[i]);
+        }
     });
 });
