861eb4951ace7d3b6565379c0b00ac522bc99797
AdrianGaudebert <adrian@gaudebert.fr>
2011-04-05 19:48:51 +0200

    Refactoring exporting method.

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var sys     = require("sys"),
-    ws      = require("websocket-server"),
-    m_game    = require("./lib/game.js"),
-    m_client  = require("./lib/client.js"),
-    m_mp      = require("./lib/message-parser.js");
+var sys             = require("sys"),
+    ws              = require("websocket-server"),
+    Game            = require("./lib/game.js"),
+    Client          = require("./lib/client.js"),
+    MessageParser   = require("./lib/message-parser.js");
 
 
 var port = 3401;
 var server = ws.createServer();
 
-var game = new m_game.Game(server).init().start();
-var parser = new m_mp.MessageParser(game);
+var game = new Game(server).init().start();
+var parser = new MessageParser(game);
 
 server.listen(port);
 
 server.addListener("connection", function(conn)
 {
-    new m_client.Client(conn, game, server, parser).init();
+    new Client(conn, game, server, parser).init();
 });
 
 sys.log("Server created. Listening on port " + port + ". ");
