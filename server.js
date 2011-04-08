9e42ad1a3b001534af1eeaf1e66e028f1879273f
AdrianGaudebert <adrian@gaudebert.fr>
2011-04-08 17:17:34 +0200

    Using Socket.io now.

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-var util    = require('util'),
-    http    = require('http'),
-    io      = require('socket.io');
+var sys             = require("util"),
+    http            = require('http'),
+    io              = require('socket.io'),
+    Game            = require("./lib/game.js"),
+    Client          = require("./lib/client.js"),
+    MessageParser   = require("./lib/message-parser.js");
 
-util.log('Starting server...');
+sys.log("Starting server... ");
+
+var port = 9309;
 
 var server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello world</h1>');
+    res.writeHead(200, {'Content-Type': 'text/html', 'Location': 'http://tetwis.lqbs.fr'});
+    res.end('');
 });
-server.listen(9309);
+
+server.listen(port);
 
 var socket = io.listen(server);
 
+var game = new Game(server).init().start();
+var parser = new MessageParser(game);
+
 socket.on('connection', function(conn){
-    util.log('Connection accepted');
+{
+    sys.log('Client connected');
+    new Client(conn, game, server, parser).init();
 });
 
-util.log('Server ready');
+sys.log("Server created. Listening on port " + port + ". ");
