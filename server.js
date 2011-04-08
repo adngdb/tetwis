e1a7b8cbbb6215ee1a02ddaa207fd875b4cf89b1
AdrianGaudebert <adrian@gaudebert.fr>
2011-04-08 16:23:44 +0200

    Bug fixed.

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var util = require('util'),
+var util    = require('util'),
     http    = require('http'),
-    io = require('socket.io');
+    io      = require('socket.io');
 
 util.log('Starting server...');
 
-server = http.createServer(function(req, res){});
-socket = io.listen(this.server);
-
+server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello world</h1>');
+});
 server.listen(9309);
+
+socket = io.listen(server);
 
 socket.on('connection', function(conn){
     util.log('Connection accepted');
-}
+});
 
 util.log('Server ready');
