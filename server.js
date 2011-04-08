82af1bc930e2215e3874e6e71bc2d81d3556cbf3
AdrianGaudebert <adrian@gaudebert.fr>
2011-04-08 16:07:40 +0200

    Commenting stuff for debug purpose.

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var sys             = require("util"),
-    ws              = require("websocket-server"),
+    ws              = require("websocket-server");/*,
     Game            = require("./lib/game.js"),
     Client          = require("./lib/client.js"),
-    MessageParser   = require("./lib/message-parser.js");
+    MessageParser   = require("./lib/message-parser.js");*/
 
 sys.log("Starting server... ");
 
 var port = 9309;
 var server = ws.createServer();
 
-var game = new Game(server).init().start();
-var parser = new MessageParser(game);
+//~ var game = new Game(server).init().start();
+//~ var parser = new MessageParser(game);
 
 server.listen(port);
 
 server.addListener("connection", function(conn)
 {
-    new Client(conn, game, server, parser).init();
+    sys.log('Client connected');
+    //~ new Client(conn, game, server, parser).init();
 });
 
 sys.log("Server created. Listening on port " + port + ". ");
