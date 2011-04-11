c135f1c796380dfe8f4f7e276fe356cfb960b3ed
AdrianGaudebert <adrian@gaudebert.fr>
2011-04-11 16:33:42 +0200

    Removing old stuff.

--- server.js
+++ server.js
@@ -54,36 +54,6 @@ http.createServer(function (request, response) {
     });
 }).listen(port);
 
-//~ var server = http.createServer(function(req, res){
-    //~ var html =  '<!DOCTYPE html><html><head><title>Tetwis ! JavaScript powered game</title>' +
-                //~ '<link rel="stylesheet" type="text/css" href="http://tetwis.lqbs.fr/css/main.css" />' +
-                //~ '<script src="http://tetwis.lqbs.fr/js/libs/load.js"></script>' +
-                //~ '<script>' +
-                //~ 'var staticFilesServer = "http://tetwis.lqbs.fr/";' +
-                //~ 'load("http://cdn.socket.io/stable/socket.io.js", staticFilesServer+"js/libs/jquery.min.js")' +
-                //~ //'.thenRun( function() { $("body").load(staticFilesServer+"content.html"); } )' +
-                //~ '.thenLoad( staticFilesServer+"js/libs/jquery.timers.js", ' +
-                //~ '       staticFilesServer+"js/libs/json2.js", ' +
-                //~ '       staticFilesServer+"js/core/config.js", ' +
-                //~ '       staticFilesServer+"js/core/cell.js", ' +
-                //~ '       staticFilesServer+"js/core/brick.js", ' +
-                //~ '       staticFilesServer+"js/core/map.js", ' +
-                //~ '       staticFilesServer+"js/core/events.js", ' +
-                //~ '       staticFilesServer+"js/core/message-parser.js", ' +
-                //~ '       staticFilesServer+"js/core/socket.js", ' +
-                //~ '       staticFilesServer+"js/core/game.js")' +
-                //~ '.then( staticFilesServer+"js/main.js");' +
-                //~ '</script>' +
-                //~ '</head><body>' +
-                //~ '<p>Loading...</p>' +
-                //~ '<ul id="log"></ul>' +
-                //~ '</body></html>';
-    //~ res.writeHead(200, {'Content-Type': 'text/html'});
-    //~ res.end(html);
-//~ });
-//~
-//~ server.listen(port);
-
 var socket = io.listen(server);
 
 var game = new Game(server).init().start();
