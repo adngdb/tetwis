3e4d0d8618559d9ad634e44aad2534311c0e6cfd
AdrianGaudebert <adrian@gaudebert.fr>
2011-04-11 15:16:01 +0200

    Cannot load from lqbs.fr.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var server = http.createServer(function(req, res){
                 '<script>' +
                 'var staticFilesServer = "http://tetwis.lqbs.fr/";' +
                 'load("http://cdn.socket.io/stable/socket.io.js", staticFilesServer+"js/libs/jquery.min.js")' +
-                '.thenRun( function() { $("body").load(staticFilesServer+"content.html"); } )' +
+                //'.thenRun( function() { $("body").load(staticFilesServer+"content.html"); } )' +
                 '.thenLoad( staticFilesServer+"js/libs/jquery.timers.js", ' +
                 '       staticFilesServer+"js/libs/json2.js", ' +
                 '       staticFilesServer+"js/core/config.js", ' +
