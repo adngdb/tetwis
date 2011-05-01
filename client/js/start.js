b553092d12a72b5e1da35ba27d0a2f6a1b206ffa
Adrian Gaudebert <adrian@gaudebert.fr>
2011-04-30 23:38:59 -0700

    Integrate loading of the js files.

--- client/js/start.js
+++ client/js/start.js
@@ -1,3 +1,15 @@
-load("js/libs/jquery.min.js").then("js/libs/socket.io.min.js", "js/core/config.js").thenRun(function() {
-
-});
+load("js/libs/jquery.min.js")
+	.then(
+		"js/libs/socket.io.min.js",
+        "js/libs/jquery.timers.js",
+        "js/libs/json2.js",
+        "js/core/config.js",
+        "js/core/cell.js",
+        "js/core/brick.js",
+        "js/core/map.js",
+        "js/core/displayer.js",
+        "js/core/events.js",
+        "js/core/message-parser.js",
+        "js/core/socket.js",
+        "js/core/game.js"
+	).then("js/main.js");
