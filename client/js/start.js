9ef450dbc95b573360bcd6cf8fb38721abce9d2e
Adrian Gaudebert <adrian@gaudebert.fr>
2011-05-01 00:34:54 -0700

    Load the login screen before the game.

--- client/js/start.js
+++ client/js/start.js
@@ -1,15 +1,39 @@
-load("js/libs/jquery.min.js")
-	.then(
-		"js/libs/socket.io.min.js",
-        "js/libs/jquery.timers.js",
-        "js/libs/json2.js",
-        "js/core/config.js",
-        "js/core/cell.js",
-        "js/core/brick.js",
-        "js/core/map.js",
-        "js/core/displayer.js",
-        "js/core/events.js",
-        "js/core/message-parser.js",
-        "js/core/socket.js",
-        "js/core/game.js"
-	).then("js/main.js");
+
+(function() {
+	var user = null;
+
+	load("js/libs/jquery.min.js")
+	.then("js/libs/jquery.tmpl.js")
+	.thenRun(function() {
+		$.get('templates/login.html', function(data) {
+			$.tmpl(data).appendTo('#content');
+
+			$('#submit-login').click(function (e) {
+				e.preventDefault();
+				var login = $('#user-login').val();
+
+				if (login != null && login != '') {
+					user = login;
+					$('#content').empty();
+					next();
+				}
+			});
+		});
+	});
+
+	function next() {
+		load("js/libs/socket.io.min.js",
+			"js/libs/jquery.timers.js",
+			"js/libs/json2.js",
+			"js/core/config.js",
+			"js/core/cell.js",
+			"js/core/brick.js",
+			"js/core/map.js",
+			"js/core/displayer.js",
+			"js/core/events.js",
+			"js/core/message-parser.js",
+			"js/core/socket.js",
+			"js/core/game.js"
+		).then("js/main.js");
+	}
+})();
