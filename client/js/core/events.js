9d12b575c78948b199dd8951d72aa1aee113e9b3
Adrian Gaudebert <adrian@gaudebert.fr>
2011-05-03 10:21:45 -0700

    Binding to this, better code.

--- client/js/core/events.js
+++ client/js/core/events.js
@@ -13,11 +13,11 @@ tetwis.Events.prototype = {
         var instance = this;
         $(this.bound).keypress(function(e) {
             switch(e.charCode || e.keyCode) {
-                case 74: case 106: instance.game.moveLeft(); break; // J
-                case 76: case 108: instance.game.moveRight(); break; // L
-                case 73: case 105: instance.game.changeShape(); break; // I
+                case 74: case 106: this.game.moveLeft(); break; // J
+                case 76: case 108: this.game.moveRight(); break; // L
+                case 73: case 105: this.game.changeShape(); break; // I
             }
             return false;
-        });
+        }.bind(this));
     }
 }
