ee5375daa6075bfb5e37b3f4b9aa56ed5ba6ac1d
Adrian Gaudebert <adrian@gaudebert.fr>
2011-05-02 19:44:16 -0700

    Initializing the Engine after new.

--- client/js/init.js
+++ client/js/init.js
@@ -1,11 +1,12 @@
 (function() {
 	// Create the Engine instance
-	tetwis.engine = new tetwis.Engine();
+	tetwis.engine = new tetwis.Engine().init();
 
 	// Load the config
 	tetwis.engine.loadConfig();
 
 	// Connect to the server
+
 	// Send the user's login
 	// Load the games list
 })();
