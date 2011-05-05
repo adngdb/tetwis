97993a276990f7c2c3013c154b056bc4404aa5d0
Adrian Gaudebert <adrian@gaudebert.fr>
2011-05-04 19:13:08 -0700

    Respecting the protocol. Still compatible with the old one.

--- client/js/core/message-parser.js
+++ client/js/core/message-parser.js
@@ -5,9 +5,35 @@ tetwis.MessageParser = function(engine) {
 tetwis.MessageParser.prototype = {
 
     parse: function(msg) {
-        var data = JSON.parse(msg);
+        var obj = JSON.parse(msg);
 
-        if (data.method == "get") {
+		if (typeof obj.type != undefined && obj.type != null) {
+			switch (obj.type) {
+				case "query":
+					this.parseQuery(obj.data);
+					break;
+				case "action":
+					this.parseAction(obj.data);
+					break;
+				case "data":
+					this.parseData(obj.data);
+					break;
+				default:
+					throw "Unknown message type"; // TODO: using an object of type Exception
+			}
+		}
+		else {
+			this.parseOld(obj);
+		}
+
+        return this;
+    },
+
+	/**
+	 * @deprecated
+	 */
+    parseOld: function(data) {
+		if (data.method == "get") {
             if (data.object == "map") {
                 this.engine.game.init(data.data);
             }
@@ -20,6 +46,61 @@ tetwis.MessageParser.prototype = {
                 this.engine.game.updatePlayersInfo(data.data);
             }
         }
+	},
+
+    parseQuery: function(data) {
+        return this;
+    },
+
+    parseAction: function(data) {
+        return this;
+    },
+
+    parseData: function(data) {
+        switch (data.method) {
+            case "new":
+                this.parseNewData(data.object, data.object_data);
+                break;
+            case "update":
+                this.parseUpdateData(data.object, data.object_data);
+                break;
+            case "delete":
+                this.parseDeleteData(data.object, data.object_data);
+                break;
+        }
+        this.gameEngine.invalidate();
+        return this;
+    },
+
+    parseNewData: function(object, data) {
+        switch (object) {
+            case "Game":
+                this.gameEngine.world.gameData(data);
+                break;
+            case "Player":
+                this.gameEngine.world.playerData(data);
+                break;
+            case "Map":
+                this.gameEngine.world.mapData(data);
+                break;
+        }
+        return this;
+    },
+
+    parseUpdateData: function(object, data) {
+        switch (object) {
+            case "Game":
+                this.gameEngine.world.gameUpdate(data);
+                break;
+            case "Player":
+                this.gameEngine.world.playerUpdate(data);
+                break;
+        }
+        return this;
+    },
+
+    parseDeleteData: function(object, data) {
+        return this;
     },
 
 }
