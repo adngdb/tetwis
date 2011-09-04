a33839dfe374eaa1af08b03fb3bd892d56c8b4eb
Adrian Gaudebert <adrian@gaudebert.fr>
2011-09-03 20:00:39 -0700

    Close bug 3 - Allowed normal behavior of unused keys, and added new keys to play (arrows, AWD, QZD and JIL can now be used)

--- client/js/core/events.js
+++ client/js/core/events.js
@@ -13,11 +13,24 @@ tetwis.Events.prototype = {
         var instance = this;
         $(this.bound).keypress(function(e) {
             switch(e.charCode || e.keyCode) {
-                case 74: case 106: this.game.moveLeft(); break; // J
-                case 76: case 108: this.game.moveRight(); break; // L
-                case 73: case 105: this.game.changeShape(); break; // I
+                case 74: case 106: // J
+                case 65: case 97: // A
+                case 81: case 113: // Q
+                case 37: // Left arrow
+                  this.game.moveLeft();
+                  return false;
+                case 76: case 108: // L
+                case 68: case 100: // D
+                case 39: // Right arrow
+                  this.game.moveRight();
+                  return false;
+                case 73: case 105: // I
+                case 87: case 119: // W
+                case 90: case 122: // Z
+                case 38: // Up arrow
+                  this.game.changeShape();
+                  return false;
             }
-            return false;
         }.bind(this));
     }
 }
